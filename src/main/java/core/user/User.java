f4e6d36cf8b5ab138dc212e93948006a025470b1
Kai7orz <kai7orz@gmail.com>
2026-01-25 16:47:01 +0900

    DBセットアップ以外のステージングfile削除

--- src/main/java/core/user/User.java
+++
@@ -1,5 +0,0 @@
-package core.user;
-
-public class User {
-    // 純粋な Userデータを定義する メソッドは null　と 空 チェックにとどめる
-}
--- src/main/java/core/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package core.user;
-
-public interface UserRepository {
-    // find など User へのDB 操作をinterface として定義する
-    void save(User user);
-    User findUserByMail(String mail);
-}
